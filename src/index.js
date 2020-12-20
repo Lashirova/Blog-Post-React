1511b10683b37fd843b3e96052903b89684f6399
Cemil Okay <cemilokay@gmail.com>
2020-12-20 23:24:49 +0000

    Iterate with map through blog posts array

--- src/index.js
+++ src/index.js
@@ -4,37 +4,51 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myBlogPost = {
-  title: 'My first blog',
-  body: 'lorem ipsum dolor sit amet..',
-  published: true
-}
-
-let classProperties;
-if(myBlogPost.published){
-  classProperties= 'green'
-} else {
-  classProperties= 'red'
-}
+const myBlogPosts = [
+  {
+    title: 'My first blog',
+    body: 'lorem ipsum dolor sit amet..',
+    published: false
+  },
+  {
+    title: 'My second blog',
+    body: 'lorem ipsum dolor sit amet..',
+    published: true
+  },
+  {
+    title: 'My third blog',
+    body: 'lorem ipsum dolor sit amet..',
+    published: false
+  },
+  {
+    title: 'My fourth blog',
+    body: 'lorem ipsum dolor sit amet..',
+    published: true
+  }
+]
 
 function publishThePost(e) {
   if(e.target.innerText === 'Publish') {
     e.target.innerText = 'Unpublish';
-    document.getElementById('title').style.color = 'green';
+    e.target.parentElement.querySelector('.blog-title').style.color = 'green';
   } else {
     e.target.innerText = 'Publish'
-    document.getElementById('title').style.color = 'red';
+    e.target.parentElement.querySelector('.blog-title').style.color = 'red';
   }
 }
 
-const blogPost = (
-  <div>
-    <h1 id="title" className={classProperties}>{myBlogPost.title}</h1>
-    <p>{myBlogPost.body}</p>
-    <button onClick={publishThePost}>Unpublish</button>
-    <img src="" alt="" />
-  </div>
-);
+const blogPost = myBlogPosts.map((myBlogPost,index) => {
+  return (
+    <div key={'post_' + index}>
+      <h1 className={(myBlogPost.published ? 'green' : 'red') + ' blog-title'}>{myBlogPost.title}</h1>
+      <p>{myBlogPost.body}</p>
+      <button onClick={publishThePost}>{myBlogPost.published ? 'Unpublish' : 'Publish'}</button>
+      <img src="" alt="" />
+      <hr/>
+    </div>
+    
+  );
+})
 
 ReactDOM.render(
   blogPost,
