53c644f6659112f83eefb3f70cd6e7d3548938aa
Cemil Okay <cemilokay@gmail.com>
2020-12-20 20:31:02 +0000

    Using JSX for adding BlogPost

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let classProperties = 'blue small-font';
+// const blueColor = 'blue';
+// const smallFont = 'small-font';
+
+const myBlogPost = {
+  title: 'My first blog',
+  body: 'lorem ipsum dolor sit amet..'
+}
+
+const blogPost = (
+  <div>
+    <h1 id="title" className={classProperties}>{myBlogPost.title}</h1>
+    <p></p>
+    <img src="" alt="" />
+  </div>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  blogPost,
   document.getElementById('root')
 );
 
@@ -15,3 +30,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+{/* <React.StrictMode>
+    <App />
+  </React.StrictMode> */}
