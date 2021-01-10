8c14de5d5dbe7052428fe4d2cac18053422891d3
Cemil Okay <cemilokay@gmail.com>
2021-01-10 23:01:50 +0000

    Getting ready for the development

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import {BlogPost} from './BlogPost';
-import Greeting from './Greeting';
 
 
 function App() {
   return (
     <div>
-      <h1>Welcome to my Blog</h1>
-      <Greeting myName="Cemil" isAdmin={false}/>
-      <BlogPost isAdmin={false}/>
+      <h1 className="text-center">Welcome to my Blog</h1>
+      
     </div>
   );
 }
