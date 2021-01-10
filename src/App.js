04bdc321fbeef6fe0d00fdab8139a6d3bfd90adf
Cemil Okay <cemilokay@gmail.com>
2021-01-10 23:11:05 +0000

    Add blog posts manually

--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ function App() {
     <div>
       <h1 className="text-center">Welcome to my Blog</h1>
       
+      <section>
+        <article>
+          <h2>Blog Post Title 1</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus repellat error, molestiae natus nesciunt quia soluta enim autem iure illo nam quae iusto. Voluptatem autem dolor, sit molestias consequatur deserunt.</p>
+        </article>
+        <article>
+          <h2>Blog Post Title 2</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus repellat error, molestiae natus nesciunt quia soluta enim autem iure illo nam quae iusto. Voluptatem autem dolor, sit molestias consequatur deserunt.</p>
+        </article>
+        <article>
+          <h2>Blog Post Title 3</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus repellat error, molestiae natus nesciunt quia soluta enim autem iure illo nam quae iusto. Voluptatem autem dolor, sit molestias consequatur deserunt.</p>
+        </article>
+        <article>
+          <h2>Blog Post Title 4</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus repellat error, molestiae natus nesciunt quia soluta enim autem iure illo nam quae iusto. Voluptatem autem dolor, sit molestias consequatur deserunt.</p>
+        </article>
+        <article>
+          <h2>Blog Post Title 5</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus repellat error, molestiae natus nesciunt quia soluta enim autem iure illo nam quae iusto. Voluptatem autem dolor, sit molestias consequatur deserunt.</p>
+        </article>
+      </section>
+      
     </div>
   );
 }
